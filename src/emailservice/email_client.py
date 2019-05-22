3151739947717ac1b261f145b3ebd5a0f4a33b29
Elizabeth Byerly <elizabeth.byerly@gmail.com>
2019-05-22 10:41:18 -0400

    emailservice predictably fails every other hour for five minutes

--- src/emailservice/email_client.py
+++ src/emailservice/email_client.py
@@ -14,38 +14,53 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from opencensus.trace.ext.grpc import client_interceptor
+from opencensus.trace.exporters import stackdriver_exporter
+from opencensus.trace.tracer import Tracer
 import grpc
-
+import time
 import demo_pb2
 import demo_pb2_grpc
 
 from logger import getJSONLogger
 logger = getJSONLogger('emailservice-client')
 
-from opencensus.trace.tracer import Tracer
-from opencensus.trace.exporters import stackdriver_exporter
-from opencensus.trace.ext.grpc import client_interceptor
 
 try:
     exporter = stackdriver_exporter.StackdriverExporter()
     tracer = Tracer(exporter=exporter)
-    tracer_interceptor = client_interceptor.OpenCensusClientInterceptor(tracer, host_port='0.0.0.0:8080')
+    tracer_interceptor = client_interceptor.OpenCensusClientInterceptor(
+        tracer, host_port='0.0.0.0:8080')
 except:
     tracer_interceptor = client_interceptor.OpenCensusClientInterceptor()
 
+
+class BurstyException(Exception):
+    pass
+
+
+def bursty_crash():
+    """Emit an error if called in the first five minutes of odd-number hours."""
+    now = time.localtime()
+    if (now.tm_hour % 2) & (now.tm_min < 5):
+        raise BurstyException('A bursty crash.')
+
+
 def send_confirmation_email(email, order):
-  channel = grpc.insecure_channel('0.0.0.0:8080')
-  channel = grpc.intercept_channel(channel, tracer_interceptor)
-  stub = demo_pb2_grpc.EmailServiceStub(channel)
-  try:
-    response = stub.SendOrderConfirmation(demo_pb2.SendOrderConfirmationRequest(
-      email = email,
-      order = order
-    ))
-    logger.info('Request sent.')
-  except grpc.RpcError as err:
-    logger.error(err.details())
-    logger.error('{}, {}'.format(err.code().name, err.code().value))
+    channel = grpc.insecure_channel('0.0.0.0:8080')
+    channel = grpc.intercept_channel(channel, tracer_interceptor)
+    stub = demo_pb2_grpc.EmailServiceStub(channel)
+    try:
+        bursty_crash()
+        stub.SendOrderConfirmation(demo_pb2.SendOrderConfirmationRequest(
+            email=email,
+            order=order
+        ))
+        logger.info('Request sent.')
+    except grpc.RpcError as err:
+        logger.error(err.details())
+        logger.error('{}, {}'.format(err.code().name, err.code().value))
+
 
 if __name__ == '__main__':
-  logger.info('Client for email service.')
+    logger.info('Client for email service.')
